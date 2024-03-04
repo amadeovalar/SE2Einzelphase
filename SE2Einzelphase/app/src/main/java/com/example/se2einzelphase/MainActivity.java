713c2672e14cbf60b7b006a4614e6d2dfe64955d
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-03-04 13:36:16 +0100

    Processing the empty input

--- SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
 //        Message from server
         messageFromServer = findViewById(R.id.serverAntwort);
     }
-    
 
-}
+//    Button behaviour function
+    public void onClickSubmit(View view){
+//        Checking if the input is empty
+        if (matrikelNummerInput.getText().toString().isEmpty()){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String noInputMsg = "Input missing!";
+//                    Create toast to tell user, that there is no input yet
+                    Toast.makeText(MainActivity.this, noInputMsg, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+}}
