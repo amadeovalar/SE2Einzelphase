e3ef9938e1300990075b1eaac39b3e9e2e56c78e
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-03-04 13:22:25 +0100

    Define the main input variables for the layout elements

--- SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
+
+    EditText matrikelNummerInput;
+
+    Integer matNr;
+
+    TextView messageFromServer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Text from user
+        matrikelNummerInput = findViewById(R.id.matrikelNummerInput);
+//        Send button
+        submitButton = findViewById(R.id.btnAbschicken);
+//        Message from server
+        messageFromServer = findViewById(R.id.serverAntwort);
     }
+    
+
 }
