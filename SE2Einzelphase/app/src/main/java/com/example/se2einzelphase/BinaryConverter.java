d9f00ce19fc657e80e05139e7d0ec9703f799841
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-03-04 21:19:55 +0100

    Finishing, reformating, refactoring in main Activity class

--- SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/BinaryConverter.java
+++ SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/BinaryConverter.java
@@ -2,12 +2,17 @@ package com.example.se2einzelphase;
 
 public class BinaryConverter {
 
-    public String convertSumToBinary(int[] numbers) {
+    public String convertSumToBinary(int numbers) {
+
+        int [] convNumbers = convertDigitsToArray(numbers);
+
         // Calculate the sum of the numbers
-        int sum = calculateSum(numbers);
+        int sum = calculateSum(convNumbers);
 
         // Convert the sum to binary
-        return Integer.toBinaryString(sum);
+        String binaryArray = toBinary(sum);
+
+        return binaryArray;
     }
 
     private int calculateSum(int[] numbers) {
@@ -18,4 +23,31 @@ public class BinaryConverter {
         return sum;
     }
 
+    public static String toBinary(int number) {
+        if (number == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (number > 0) {
+            binary.insert(0, number % 2);
+            number /= 2;
+        }
+        return binary.toString();
+    }
+
+    public int[] convertDigitsToArray(int n) {
+
+        int [] temp = new int[String.valueOf(n).length()]; // Calculate the length of digits
+        int i = String.valueOf(n).length()-1 ;  // Initialize the value to the last index
+
+        do {
+            temp[i] = n % 10;
+            n = n / 10;
+            i--;
+        } while(n>0);
+
+        return temp;
+    }
 }
+
--- SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.se2einzelphase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,19 +8,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
+    Button calculateButton;
 
     EditText matrikelNummerInput;
 
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView messageFromServer;
 
-//    Server name and port
+    //    Server name and port
     String serverName = "se2-submission.aau.at";
     Integer portNr = 20080;
 
@@ -44,74 +42,103 @@ public class MainActivity extends AppCompatActivity {
         matrikelNummerInput = findViewById(R.id.matrikelNummerInput);
 //        Send button
         submitButton = findViewById(R.id.btnAbschicken);
+//        Calculate button
+        calculateButton = findViewById(R.id.btnBerechnen);
 //        Message from server
         messageFromServer = findViewById(R.id.serverAntwort);
     }
 
-//    Button behaviour function
-    public void onClickSubmit(View view){
+    //    Submit button behaviour method
+    public void onClickSubmit(View view) {
 //        Checking if the input is empty
-        if (matrikelNummerInput.getText().toString().isEmpty()){
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    String noInputMsg = "Input missing!";
+        if (matrikelNummerInput.getText().toString().isEmpty()) {
+            runOnUiThread(() -> {
+                String noInputMsg = "Input missing!";
 //                    Create toast to tell user, that there is no input yet
-                    Toast.makeText(MainActivity.this, noInputMsg, Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(MainActivity.this, noInputMsg, Toast.LENGTH_SHORT).show();
             });
-        } else{
+        } else {
             submitButton.setEnabled(true);
             matNr = Integer.parseInt(matrikelNummerInput.getText().toString());
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
+            new Thread(() -> {
 //                    Establish the connection to the server by creating a socket
-                    Socket socket = null;
-                    try {
-                        socket = new Socket(serverName, portNr);
-                        Log.v("Connected", socket.getRemoteSocketAddress().toString());
+                Socket socket = null;
+                try {
+                    socket = new Socket(serverName, portNr);
+                    Log.v("Connected", socket.getRemoteSocketAddress().toString());
 
-                        // Get output stream from the socket
-                        PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
-                        toServer.println(matNr);
+                    // Get output stream from the socket
+                    PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
+                    toServer.println(matNr);
 
-                        // Send data
-                        Log.v("MatNrStr", String.valueOf(matNr));
+                    // Send data
+                    Log.v("MatNrStr", String.valueOf(matNr));
 
 //                        Reading the data from the server
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        String serverMessage = bufferedReader.readLine();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String serverMessage = bufferedReader.readLine();
 
-                        Log.v("EditText", matrikelNummerInput.getText().toString());
-                        Log.v("ServerMessage", serverMessage);
+                    Log.v("EditText", matrikelNummerInput.getText().toString());
+                    Log.v("ServerMessage", serverMessage);
 
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
+                    runOnUiThread(() -> {
 //                                Turning text to visible
-                                messageFromServer.setVisibility(View.VISIBLE);
+                        messageFromServer.setVisibility(View.VISIBLE);
 //                                Show the message from server
-                                messageFromServer.setText(serverMessage);
-                            }
-                        });
+                        messageFromServer.setText(serverMessage);
+                    });
 
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    } finally {
-                        // Close the socket in the finally block to ensure it's always closed
-                        if (socket != null) {
-                            try {
-                                socket.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    // Close the socket in the finally block to ensure it's always closed
+                    if (socket != null) {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
                 }
             }).start();
 
         }
+    }
 
-}}
+    //  Calculate button behavior method
+    public void onClickCalculateBinary(View view) {
+        if (matrikelNummerInput.getText().toString().isEmpty()) {
+            runOnUiThread(() -> {
+                String noInputMsg = "Input missing!";
+//                    Create toast to tell user, that there is no input yet
+                Toast.makeText(MainActivity.this, noInputMsg, Toast.LENGTH_SHORT).show();
+            });
+        } else {
+//            Enable the button
+            calculateButton.setEnabled(true);
+            matNr = Integer.parseInt(matrikelNummerInput.getText().toString());
+
+            try {
+                // Create an instance of BinaryConverter
+                BinaryConverter binaryConverter = new BinaryConverter();
+
+                // Convert the sum of numbers to binary
+                String binaryArray = binaryConverter.convertSumToBinary(matNr);
+
+                // Print the result
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //                                Turning text to visible
+                        messageFromServer.setVisibility(View.VISIBLE);
+                        //                                show the results of the calculation
+                        messageFromServer.setText(binaryArray);
+                    }
+                });
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
