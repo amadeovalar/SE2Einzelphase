bc4c325e6011826237bb6fccf3553f9ba5cd4cc0
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-03-04 13:48:15 +0100

    Handling server connection and response

--- SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ SE2Einzelphase/app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     Integer matNr;
 
     TextView messageFromServer;
+
+//    Server name and port
+    String serverName = "se2-submission.aau.at";
+    Integer portNr = 20080;
 
 
     @Override
@@ -45,6 +57,45 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, noInputMsg, Toast.LENGTH_SHORT).show();
                 }
             });
+        } else{
+            submitButton.setEnabled(true);
+            matNr = Integer.parseInt(matrikelNummerInput.getText().toString());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+//                    Establish the connection to the server by creating a socket
+                    try {
+                        Socket socket = new Socket(serverName, portNr);
+                        Log.v("Connected", socket.getRemoteSocketAddress().toString());
+
+//                        Sending data to server
+                        PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
+                        toServer.println(matNr);
+
+//                        Reading the data from the server
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String serverMessage = bufferedReader.readLine();
+
+                        Log.v("EditText", matrikelNummerInput.getText().toString());
+                        Log.v("ServerMessage", serverMessage);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+//                                Turning text to visible
+                                messageFromServer.setVisibility(View.VISIBLE);
+//                                Show the message from server
+                                messageFromServer.setText(serverMessage);
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
         }
 
 }}
